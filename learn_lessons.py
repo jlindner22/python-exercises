e2276c36257cd6b074e4d5c871592f1cc65d7ebc
jlindner22 <jenlindner22@gmail.com>
2020-05-13 14:36:31 -0400

    learn lessons strings added

---
+++ learn_lessons.py
@@ -0,0 +1,20 @@
+# print('uppercase test'.upper())
+# print('LOWERCASE THIS'.lower())
+# print("Lowercase".endswith('e'))
+# print('Lowercase'.replace('e', '@'))
+# # help(str)
+# print('jen is learning python'.capitalize())
+# print('jen is learning python'.title())
+
+print(type("10.34"))
+
+print("art.vandelay@vandelay.co".endswith('.com'))
+
+van = 'vandelay.com'
+web = 'www.'
+print(web + van)
+
+#slices first 3 characters of string
+print("7286663334"[:3])
+
+
