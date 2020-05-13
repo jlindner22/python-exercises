81c6df8b41fed6660642ca161963c88340a2b858
jlindner22 <jenlindner22@gmail.com>
2020-05-13 13:48:24 -0400

    more lessons added

--- ex1.py
+++ ex1.py
@@ -85,15 +85,14 @@
 # print(end7 + end8 + end9 + end10 + end11 + end12)
 
 #8
-# formatter = "{} {} {} {}"
+formatter = "{} {} {} {}"
 
-# print(formatter.format(1, 2, 3, 4))
-# print(formatter.format("one", "two", "three", "four"))
-# print(formatter.format(True, False, False, True))
-# print(formatter.format(formatter, formatter, formatter, formatter))
-# print(formatter.format(
-#     "Roses are red", "Violets are blue", "You're reading my repo", "and I did that too."
-# ))
+print(formatter.format(1, 2, 3, 4))
+print(formatter.format("one", "two", "three", "four"))
+print(formatter.format(True, False, False, True))
+print(formatter.format(formatter, formatter, formatter, formatter))
+print(formatter.format(
+    "Roses are red", "Violets are blue", "You're reading my repo", "and I did that too."
+))
 
-#9
 
